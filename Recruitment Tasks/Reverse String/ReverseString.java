d4c04bb50bad3210eec4b95330eb3a5581fbaa0e
Patryk <109095669+patryk47853@users.noreply.github.com>
2023-10-13 21:01:46 +0200

    Add task solution

---
+++ Recruitment Tasks/Reverse String/ReverseString.java
@@ -0,0 +1,16 @@
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void reverseString(char[] s) {
+        int length = s.length;
+
+        for(int i = 0; i < length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[length - 1 - i];
+            s[length - 1 - i] = temp;
+        }
+    }
+}
